f07a2002395625b64a6ccb85b103676a9b04d816
alfandi <fandidixpc1@gmail.com>
2023-04-07 09:12:28 +0700

    Add project files.

---
+++ easd_1_152_D/easd_1_152_D.cpp
@@ -0,0 +1,15 @@
+// 1. Karena algoritma merupakan langkah - langkah dalam menyelesaikan masalah secara urut
+// 2. Static dan Dynamic
+// 3. Kecepatan mesin, Sistem operasi, Ukuran masukkan
+// 4. Algorithma yang efisien menerut saya apabila datanya kecil adalah insertionsort karena algorithma tersebut penggunaannya sederhana dan stabil
+// 5. 
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int fandi[72];
+int n;
